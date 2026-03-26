89c98fb5c509dcbc77b76f74a204fc55b084b08d
actualizarcabel2-prog <actualizarcabel2@gmail.com>
2026-03-26 09:34:25 +0100

    Fix bloqueo: socket.setdefaulttimeout + threading para resolver

--- plugin.video.matelotricinema/lib/resolver.py
+++ plugin.video.matelotricinema/lib/resolver.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
-"""Resolver Matelotri Cinema v7 - urllib puro + Torrentio + AllDebrid."""
+"""Resolver Matelotri v8 - socket timeout global + threading."""
 import json
 import time
+import socket
+import threading
 
 try:
     from urllib.request import urlopen, Request
@@ -10,16 +12,19 @@ except ImportError:
     from urllib2 import urlopen, Request
     from urllib import quote
 
+# FORZAR timeout global en TODOS los sockets (Android fix)
+socket.setdefaulttimeout(5)
+
 AD_KEY = "i5MI5R32vKVfOk3v46WA"
 AD_AGENT = "matelotri"
 AD_BASE = "https://api.alldebrid.com/v4"
 
 
-def _get(url, timeout=4):
+def _get(url):
     try:
         req = Request(url, headers={"User-Agent": "Kodi/21.2",
                                      "Accept": "application/json"})
-        resp = urlopen(req, timeout=timeout)
+        resp = urlopen(req, timeout=4)
         return json.loads(resp.read().decode("utf-8"))
     except Exception:
         return {}
@@ -27,28 +32,42 @@ def _get(url, timeout=4):
 
 def _ad(endpoint, extra=""):
     return _get("{}/{}?agent={}&apikey={}{}".format(
-        AD_BASE, endpoint, AD_AGENT, AD_KEY, extra), 6)
+        AD_BASE, endpoint, AD_AGENT, AD_KEY, extra))
 
 
 def resolve_movie(title, year=None, tmdb_id=None, imdb_id=None):
     if not imdb_id:
         return []
-    magnets = _torrentio_movie(imdb_id)
+    # Buscar magnets con timeout via threading
+    magnets = []
+    t = threading.Thread(target=lambda: magnets.extend(_torrentio_movie(imdb_id)))
+    t.daemon = True
+    t.start()
+    t.join(timeout=6)  # Maximo 6 seg
     if not magnets:
-        magnets = _yts(imdb_id)
+        # Intentar YTS
+        t2 = threading.Thread(target=lambda: magnets.extend(_yts(imdb_id)))
+        t2.daemon = True
+        t2.start()
+        t2.join(timeout=5)
     return _resolve(magnets)
 
 
 def resolve_episode(title, season, episode, tmdb_id=None, imdb_id=None):
     if not imdb_id:
         return []
-    magnets = _torrentio_episode(imdb_id, int(season), int(episode))
+    magnets = []
+    t = threading.Thread(target=lambda: magnets.extend(
+        _torrentio_episode(imdb_id, int(season), int(episode))))
+    t.daemon = True
+    t.start()
+    t.join(timeout=6)
     return _resolve(magnets)
 
 
 def _torrentio_movie(imdb_id):
     magnets = []
-    data = _get("https://torrentio.strem.fun/stream/movie/{}.json".format(imdb_id), 5)
+    data = _get("https://torrentio.strem.fun/stream/movie/{}.json".format(imdb_id))
     for s in data.get("streams", [])[:8]:
         h = s.get("infoHash", "")
         if h:
@@ -61,7 +80,7 @@ def _torrentio_movie(imdb_id):
 def _torrentio_episode(imdb_id, s, e):
     magnets = []
     data = _get("https://torrentio.strem.fun/stream/series/{}:{}:{}.json".format(
-        imdb_id, s, e), 5)
+        imdb_id, s, e))
     for st in data.get("streams", [])[:8]:
         h = st.get("infoHash", "")
         if h:
@@ -73,8 +92,7 @@ def _torrentio_episode(imdb_id, s, e):
 
 def _yts(imdb_id):
     magnets = []
-    data = _get("https://yts.mx/api/v2/list_movies.json?query_term={}&limit=1".format(
-        imdb_id), 4)
+    data = _get("https://yts.mx/api/v2/list_movies.json?query_term={}&limit=1".format(imdb_id))
     for m in data.get("data", {}).get("movies", [])[:1]:
         for t in m.get("torrents", []):
             h = t.get("hash", "")
@@ -104,16 +122,21 @@ def _sz(text):
 
 def _resolve(magnets):
     links = []
-    for m in magnets[:4]:
+    for m in magnets[:3]:
+        result = [None]
         magnet = "magnet:?xt=urn:btih:{}".format(m["hash"])
-        url = _ad_stream(magnet)
+        t = threading.Thread(target=lambda mg=magnet, r=result: r.__setitem__(0, _ad_stream(mg)))
+        t.daemon = True
+        t.start()
+        t.join(timeout=8)
+        url = result[0]
         if url:
             links.append({
                 "name": "[COLOR gold]{} {}[/COLOR] ({})".format(
                     m["quality"], m["name"], m.get("size", "")),
                 "url": url, "quality": m["quality"],
                 "lang": "multi", "source": "alldebrid"})
-            if len(links) >= 3:
+            if len(links) >= 2:
                 break
     return links
 
@@ -131,7 +154,7 @@ def _ad_stream(magnet):
             return None
         for i in range(3):
             if i > 0:
-                time.sleep(1.5)
+                time.sleep(1)
             st = _ad("magnet/status", "&id={}".format(mid))
             mg = st.get("data", {}).get("magnets", {})
             if mg.get("statusCode") == 4:
